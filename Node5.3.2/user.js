406e8c16304f167ac604cff2a08f6626a9c93564
Mansi Ghalke <102944118+mansi-30@users.noreply.github.com>
2022-07-08 12:48:18 +0530

    Create user.js

---
+++ Node5.3.2/user.js
@@ -0,0 +1,33 @@
+const users =  [
+    {
+        "id":1,
+        "login":"mansiiii",
+        "password":"mansi@123",
+        "age":21,
+        "isDeleted":false
+    },
+    {
+        "id":2,
+        "login":"alexa",
+        "password":"alexa@123",
+        "age":16,
+        "isDeleted":false
+    },
+    {
+        "id":3,
+        "login":"abc",
+        "password":"abc@123",
+        "age":22,
+        "isDeleted":false
+    },
+    {
+        "id":4,
+        "login":"john",
+        "password":"john@123",
+        "age":28,
+        "isDeleted":false
+    }
+];
+module.exports = {
+    users : users,
+}
